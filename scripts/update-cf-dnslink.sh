01d8425eb8881fa01f396362f9b8b653e11e5a7d
Kebin Liu <lkebin@gmail.com>
2023-03-03 19:43:02 +0800

    POSIX compatibility

--- scripts/update-cf-dnslink.sh
+++ scripts/update-cf-dnslink.sh
@@ -9,11 +9,11 @@ hostnames=$(curl -s \
     -H "Content-Type: application/json" \
     "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/web3/hostnames")
 
-for i in $(echo $hostnames |jq -r -c '.result | .[]'); do
-    target=$(echo $i |jq -r -c '.target')
-    name=$(echo $i |jq -r -c '.name')
-    id=$(echo $i |jq -r -c '.id')
-    if [ $target == "ipfs" -a $name == "$DOMAIN" ]; then
+for i in $(echo "$hostnames" |jq -r -c '.result | .[]'); do
+    target=$(echo "$i" |jq -r -c '.target')
+    name=$(echo "$i" |jq -r -c '.name')
+    id=$(echo "$i" |jq -r -c '.id')
+    if [ "$target" = "ipfs" ] && [ "$name" = "$DOMAIN" ]; then
         curl -s \
             -X PATCH \
             -H "Authorization: Bearer ${CF_API_TOKEN}" \
