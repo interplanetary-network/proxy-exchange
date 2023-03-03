9c0db22394e4f14b1b32452b5d07148b14e6ea32
Kebin Liu <lkebin@gmail.com>
2023-03-03 19:24:03 +0800

    Fixes dnslink update script

---
+++ scripts/update-cf-dnslink.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DOMAIN=$1
+DNSLINK=$2
+
+hostnames=$(curl -s \
+    -X GET \
+    -H "Authorization: Bearer ${CF_API_TOKEN}" \
+    -H "Content-Type: application/json" \
+    "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/web3/hostnames")
+
+for i in $(echo $hostnames |jq -r -c '.result | .[]'); do
+    target=$(echo $i |jq -r -c '.target')
+    name=$(echo $i |jq -r -c '.name')
+    id=$(echo $i |jq -r -c '.id')
+    if [[ $target == "ipfs" && $name == "$DOMAIN" ]]; then
+        curl -s \
+            -X PATCH \
+            -H "Authorization: Bearer ${CF_API_TOKEN}" \
+            -H "Content-Type: application/json" \
+            -d "{\"dnslink\":\"${DNSLINK}\"}" \
+            "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/web3/hostnames/${id}" | jq -r -c '.result.status'
+    fi
+done
